c14b9f8a6f14b123223cac981c5ec01d21d79717
Ajay Kamble <ajay.kamble@springernature.com>
2018-02-07 16:39:34 +0530

    diagonal moves for king

--- src/chess_tg/moves.clj
+++ src/chess_tg/moves.clj
@@ -6,4 +6,4 @@
         left  (dec col)
         top (inc row)
         bottom (dec row)]
-    #{[left row] [right row] [col bottom] [col top]}))
+    #{[left row] [right row] [col bottom] [col top] [left bottom] [right bottom] [left top] [right top]}))
