44205b2c521ea9d148b58db4a8971ed6c34e58b5
Ajay Kamble <ajay.kamble@springernature.com>
2018-02-07 16:48:50 +0530

    separate fns for king and knight

--- src/chess_tg/moves.clj
+++ src/chess_tg/moves.clj
@@ -1,9 +1,19 @@
 (ns chess-tg.moves)
 
-(defn moves
-  [piece [col row]]
+(defn king-moves
+  [col row]
   (let [right (inc col)
         left  (dec col)
         top (inc row)
         bottom (dec row)]
     #{[left row] [right row] [col bottom] [col top] [left bottom] [right bottom] [left top] [right top]}))
+
+(defn knight-moves
+  [col row]
+  #{[6 5] [4 5]})
+
+(defn moves
+  [piece [col row]]
+  (condp = piece
+    :king (king-moves col row)
+    :knight (knight-moves col row)))
