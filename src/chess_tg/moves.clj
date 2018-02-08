aa3337ce50cef7e8b2db39580240ebccf8c7105d
Ajay Kamble <ajay.kamble@springernature.com>
2018-02-08 08:27:33 +0530

    Added right and top right that work on D5 instead of 54

--- src/chess_tg/moves.clj
+++ src/chess_tg/moves.clj
@@ -1,35 +1,57 @@
 (ns chess-tg.moves
   "model to represent chess moves. rows are called ranks - and are numbered 1 to 8. columns are files and are from a to h")
 
-(defn square [file rank]
+(def files ["A" "B" "C" "D" "E" "F" "G" "H"])
+
+(defn square
+  [file rank]
   (str file rank))
 
-(defn char-to-int [c]
+(defn char-to-int
+  [c]
   (Integer/parseInt (str c)))
 
-(defn go-right [col]
+(defn go-right
+  [col]
   (inc col))
 
-(defn go-left [col]
+(defn go-left
+  [col]
   (dec col))
 
-(defn top [[file rank-char]]
+(defn top
+  [[file rank-char]]
   (let [rank (char-to-int rank-char)]
     (square file (inc rank))))
 
-(defn bottom [[file rank-char]]
+(defn bottom
+  [[file rank-char]]
   (let [rank (char-to-int rank-char)]
     (square file (dec rank))))
 
-(defn go-top-by [col step]
+(defn right
+  [[file rank]]
+  (let [index-of-file (.indexOf files (str file))]
+    (square (files (inc index-of-file)) rank)))
+
+(defn top-right
+  [[file rank-char]]
+  (let [index-of-file (.indexOf files (str file))
+        next-file (files (inc index-of-file))
+        next-rank (inc (char-to-int rank-char))]
+    (square next-file next-rank)))
+
+(defn go-top-by
+  [col step]
   nil)
 
-(defn go-bottom [row]
+(defn go-bottom
+  [row]
   (dec row))
 
 (defn king-moves
   [square]
-  #{(top square) "E6" "E5" "E4" (bottom square) "C4" "C5" "C6"})
+  #{(top square) (top-right square) (right square) "E4" (bottom square) "C4" "C5" "C6"})
 
 (defn knight-moves
   [col row]
