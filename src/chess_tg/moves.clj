80d4d7cf5b35318ac106a1d67c4948739a5f5cc8
Ajay Kamble <ajay.kamble@springernature.com>
2018-02-08 16:51:45 +0530

    removed redundant functions, using new functions for king moves

--- src/chess_tg/moves.clj
+++ src/chess_tg/moves.clj
@@ -15,67 +15,20 @@
   [[file rank]]
   (square file (r/top rank)))
 
-(defn top-right
-  "Takes a `file` and a `rank` and returns the square that is at the top right."
-  [[file rank]]
-  (let [right-file (f/right file)
-        top-rank (r/top rank)]
-    (square right-file top-rank)))
-
 (defn right
   "Takes a `file` and a `rank` and returns the square that is at the right."
   [[file rank]]
   (square (f/right file) rank))
-
-(defn bottom-right
-  "Takes a `file` and a `rank` and returns the square that is at the bottom right."
-  [[file rank]]
-  (let [right-file (f/right file)
-        bottom-rank (r/bottom rank)]
-    (square right-file bottom-rank)))
 
 (defn bottom
   "Takes a `file` and a `rank` and returns the square that is at the bottom."
   [[file rank]]
   (square file (r/bottom rank)))
 
-(defn bottom-left
-  "Takes a `file` and a `rank` and returns the square that is at the bottom left."
-  [[file rank]]
-  (let [left-file (f/left file)
-        bottom-rank (r/bottom rank)]
-    (square left-file bottom-rank)))
-
 (defn left
   "Takes a `file` and a `rank` and returns the square that is at the left."
   [[file rank]]
   (square (f/left file) rank))
-
-(defn top-left
-  "Takes a `file` and a `rank` and returns the square that is at the top left."
-  [[file rank]]
-  (let [left-file (f/left file)
-        top-rank (r/top rank)]
-    (square left-file top-rank)))
-
-(defn king-moves
-  "Takes a `square` occupied by a king, and returns all the squares it can occupy on the next move."
-  [square]
-  #{(top square) (top-right square) (right square) (bottom-right square) (bottom square) (bottom-left square) (left square) (top-left square)})
-
-(defn top-by-2-then-right
-  "Returns the square after going 2 ranks up and to right file, given a `file` and a `rank`."
-  [[file rank]]
-  (let [right-file (f/right file)
-        top-by-2-rank (r/top-by 2 rank)]
-    (square right-file top-by-2-rank)))
-
-(defn right-by-2-then-top
-  "Returns the square after going 2 files right and then 1 rank top, given a `file` and a `rank`."
-  [[file rank]]
-  (let [right-by-2-file (f/right-by 2 file)
-        top-rank (r/top rank)]
-    (square right-by-2-file top-rank)))
 
 (defn go-to
   "Returns the square that can be reached by following `next-move` from current `square`."
@@ -90,6 +43,30 @@
   "Takes a `square` and one or more `moves` and returns the square that can be reached by following `moves`."
   [square & moves]
   (reduce go-to square moves))
+
+(defn squares-for-moves
+  "Takes a sequence of `moves` and a `square`, and returns all squares that can be reached by following each move."
+  [square moves]
+  (into #{} (map #(apply square-at square %1) moves)))
+
+(defn king-moves
+  "Takes a `square` occupied by a king, and returns all the squares it can occupy on the next move."
+  [square]
+  (squares-for-moves square [[:top] [:top :right] [:right] [:bottom :right] [:bottom] [:bottom :left] [:left] [:top :left]]))
+
+(defn top-by-2-then-right
+  "Returns the square after going 2 ranks up and to right file, given a `file` and a `rank`."
+  [[file rank]]
+  (let [right-file (f/right file)
+        top-by-2-rank (r/top-by 2 rank)]
+    (square right-file top-by-2-rank)))
+
+(defn right-by-2-then-top
+  "Returns the square after going 2 files right and then 1 rank top, given a `file` and a `rank`."
+  [[file rank]]
+  (let [right-by-2-file (f/right-by 2 file)
+        top-rank (r/top rank)]
+    (square right-by-2-file top-rank)))
 
 (defn knight-moves
   "Takes a `square`, and returns all the possible moves for a knight."
