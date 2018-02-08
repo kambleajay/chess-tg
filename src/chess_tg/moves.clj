081a04c137ff268cd4e1cc1f3a9bda67d0bd2a4d
Ajay Kamble <ajay.kamble@springernature.com>
2018-02-08 08:37:10 +0530

    removing old functions

--- src/chess_tg/moves.clj
+++ src/chess_tg/moves.clj
@@ -11,14 +11,6 @@
   [c]
   (Integer/parseInt (str c)))
 
-(defn go-right
-  [col]
-  (inc col))
-
-(defn go-left
-  [col]
-  (dec col))
-
 (defn top
   [[file rank-char]]
   (let [rank (char-to-int rank-char)]
@@ -49,24 +41,16 @@
         right-rank (inc (char-to-int rank-char))]
     (square top-file right-rank)))
 
-(defn go-top-by
-  [col step]
-  nil)
-
-(defn go-bottom
-  [row]
-  (dec row))
-
 (defn king-moves
   [square]
   #{(top square) (top-right square) (right square) (bottom-right square) (bottom square) "C4" "C5" "C6"})
 
-(defn knight-moves
-  [col row]
-  (let [right (go-right col)
-        top (inc (inc row))
-        left (go-left col)]
-    #{[right top] [left top]}))
+(comment (defn knight-moves
+           [col row]
+           (let [right (go-right col)
+                 top (inc (inc row))
+                 left (go-left col)]
+             #{[right top] [left top]})))
 
 (defn moves
   [piece square]
