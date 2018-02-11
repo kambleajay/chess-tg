e638fde4082c631bc914007e48637c2a67a8a469
Ajay Kamble <ajay.kamble@springernature.com>
2018-02-11 17:27:12 +0530

    removed boilerplate clutter from moves test with defmovetest macro

--- test/chess_tg/moves_test.clj
+++ test/chess_tg/moves_test.clj
@@ -2,44 +2,39 @@
   (:require [clojure.test :refer :all]
             [chess-tg.moves :refer :all]))
 
-(deftest test-top
-  (are [square expected]
-      (= (top square) expected)
-    "E4" "E5"
-    "H1" "H2"
-    "C7" "C8"))
+(defmacro defmovetest
+  [name-of-test f & square-and-expected-seq]
+  `(deftest ~name-of-test
+     (are [square# expected#]
+         (= (~f square#) expected#)
+       ~@square-and-expected-seq)))
 
-(deftest test-bottom
-  (are [square expected]
-      (= (bottom square) expected)
-    "C3" "C2"
-    "H8" "H7"
-    "F4" "F3"))
+(defmovetest test-top top
+  "E4" "E5"
+  "H1" "H2"
+  "C7" "C8")
 
-(deftest test-right
-  (are [square expected]
-      (= (right square) expected)
-    "D4" "E4"
-    "A1" "B1"
-    "G8" "H8"))
+(defmovetest test-bottom bottom
+  "C3" "C2"
+  "H8" "H7"
+  "F4" "F3")
 
-(deftest test-left
-  (are [square expected]
-      (= (left square) expected)
-    "B1" "A1"
-    "F5" "E5"
-    "C7" "B7"))
+(defmovetest test-right right
+  "D4" "E4"
+  "A1" "B1"
+  "G8" "H8")
 
-(deftest test-top-right
-  (are [square expected]
-      (= (top-right square) expected)
-    "E4" "F5"
-    "A1" "B2"
-    "D6" "E7"))
+(defmovetest test-left left
+  "B1" "A1"
+  "F5" "E5"
+  "C7" "B7")
 
-(deftest test-bottom-right
-  (are [square expected]
-      (= (bottom-right square) expected)
-    "B2" "C1"
-    "G4" "H3"
-    "D4" "E3"))
+(defmovetest test-top-right top-right
+  "E4" "F5"
+  "A1" "B2"
+  "D6" "E7")
+
+(defmovetest test-bottom-right bottom-right
+  "B2" "C1"
+  "G4" "H3"
+  "D4" "E3")
